fd56a53aafa84cfeace8bb6d6e38501062df7496
Jos van der Til <jos@vandertil.net>
2019-10-15 23:40:53 +0200

    Add hugo script

---
+++ hugo.ps1
@@ -0,0 +1,27 @@
+$hugoVersion = "0.58.3"
+$hugoFilename = "hugo_extended_$hugoVersion`_Windows-64bit.zip"
+$hugoRelease = "https://github.com/gohugoio/hugo/releases/download/v$hugoVersion/$hugoFilename"
+$destination = Join-Path $PSScriptRoot ".bin"
+
+function Download-File($Url, $Destination)
+{
+    try
+    {
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($Url, $Destination)
+    } finally {
+        $wc.Dispose()
+    }
+}
+
+mkdir $destination -ErrorAction SilentlyContinue
+
+if( (Test-Path (Join-Path $destination $hugoFilename)) -eq $false )
+{
+    Download-File -Url $hugoRelease -Destination (Join-Path $destination $hugoFilename)
+    Expand-Archive -Path (Join-Path $destination $hugoFilename) -Destination $destination -Force
+}
+
+$hugoExe = (Join-Path $destination hugo.exe)
+
+& $hugoExe $args
