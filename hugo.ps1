2d057c2ef6036eb2a910eb08cc4f25f515eaced0
Jos van der Til <jos@vandertil.net>
2019-10-25 16:19:13 +0200

    Upgrade Hugo to 0.59.0

--- hugo.ps1
+++ hugo.ps1
@@ -1,4 +1,4 @@
-$hugoVersion = "0.58.3"
+$hugoVersion = "0.59.0"
 $hugoFilename = "hugo_extended_$hugoVersion`_Windows-64bit.zip"
 $hugoRelease = "https://github.com/gohugoio/hugo/releases/download/v$hugoVersion/$hugoFilename"
 $destination = Join-Path $PSScriptRoot ".bin"
