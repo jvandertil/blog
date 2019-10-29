8248429984ed47dfef23f1762a179823d2a0866c
Jos van der Til <jos@vandertil.net>
2019-10-30 00:21:43 +0100

    Add darkmode style, scripts, and button

---
+++ assets/js/darkmode.js
@@ -0,0 +1,73 @@
+function toggleStylesheet(dark) {
+    var stylesheetName;
+
+    if (dark) {
+        stylesheetName = "vs.min.css";
+    } else {
+        stylesheetName = "vs2015.min.css";
+    }
+
+    var styleSheets = document.styleSheets;
+
+    for (var i = 0; i < styleSheets.length; ++i) {
+        var ss = styleSheets[i];
+
+        if (ss.href) {
+            if (ss.href.indexOf("highlight") >= 0) {
+                if (ss.href.endsWith(stylesheetName)) {
+                    ss.disabled = true;
+                } else {
+                    ss.disabled = false;
+                }
+            }
+        }
+    }
+}
+
+function isDarkmodeEnabled() {
+    var body = document.getElementsByTagName("body")[0];
+    var darkModeEnabled = body.classList.contains('dark');
+
+    return darkModeEnabled;
+}
+
+function saveDarkmodePreference(enabled) {
+    localStorage.setItem("darkmode-enabled", enabled.toString());
+}
+
+function applyDarkmode() {
+    var enabled = localStorage.getItem("darkmode-enabled");
+    var body = document.getElementsByTagName("body")[0];
+
+    if (enabled == null) {
+        enabled = isDarkmodeEnabled().toString();
+    }
+
+    if (enabled === "true") {
+        body.classList.add('dark');
+        toggleStylesheet(true);
+    } else {
+        body.classList.remove('dark');
+        toggleStylesheet(false);
+    }
+}
+
+function toggleDarkmode() {
+    saveDarkmodePreference(!isDarkmodeEnabled());
+
+    applyDarkmode();
+}
+
+function toggleDarkmodeKeyPress(event) {
+    var KEY_ENTER = 13;
+    var KEY_SPACE = 32;
+
+    switch (event.which) {
+        case KEY_ENTER:
+        case KEY_SPACE: {
+            toggleDarkmode();
+        }
+    }
+}
+
+applyDarkmode();
