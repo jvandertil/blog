07b1a5613adae0540dca3505a045a5d79459d06b
Jos van der Til <jos@vandertil.net>
2020-04-02 13:11:39 +0200

    Add minify to serve script

--- hugo-serve.cmd
+++ hugo-serve.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -File .\hugo.ps1 serve -DEF
+powershell -ExecutionPolicy ByPass -NoProfile -File .\hugo.ps1 serve -DEF --minify
 exit /b %ERRORLEVEL%
