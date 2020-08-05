22d872db4f82f030dca992d8debc6f72c38c47c8
Jos van der Til <jos@vandertil.net>
2020-08-05 18:00:37 +0200

    Switch infra build to linux

---
+++ eng/build-infra.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+BUILD_CONFIGURATION="Release"
+
+DIRECTORY=$(dirname `realpath $0`)
+ROOT="$DIRECTORY/.."
+
+ARTIFACTS="$ROOT/artifacts"
+INFRA_ARTIFACT="$ARTIFACTS/infra"
+
+INFRA_SOURCE="$ROOT/infra"
+INFRA_PROJECT="$INFRA_SOURCE/blog.csproj"
+
+mkdir -p $INFRA_ARTIFACT
+
+dotnet restore $INFRA_PROJECT
+dotnet build --no-restore -c $BUILD_CONFIGURATION $INFRA_PROJECT
+dotnet publish --no-build -c $BUILD_CONFIGURATION -o $INFRA_ARTIFACT $INFRA_PROJECT
