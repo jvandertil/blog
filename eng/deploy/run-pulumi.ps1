572fd67bad72419522570b3dc82fa9076e8175c0
Jos van der Til <jos@vandertil.net>
2020-08-11 09:14:47 +0200

    Bump Pulumi.Azure plugin to 3.15.0

--- eng/deploy/run-pulumi.ps1
+++ eng/deploy/run-pulumi.ps1
@@ -5,7 +5,7 @@ param(
 
 . $PSScriptRoot\helpers.ps1
 
-$azurePluginVersion = "v3.14.0"
+$azurePluginVersion = "v3.15.0"
 $cloudFlarePluginVersion = "v2.4.0"
 
 Exec { & pulumi plugin install resource azure $azurePluginVersion | Write-Host } "Error installing azure plugin"
