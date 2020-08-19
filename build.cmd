668ad5ae2bb79d31ab0772dfec6cf8b434abbd13
Jos van der Til <jos@vandertil.net>
2020-08-19 11:41:49 +0200

    Add clean step to build.cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 @echo off
 
-powershell -ExecutionPolicy ByPass -NoProfile -File .\eng\build-blog.ps1
-powershell -ExecutionPolicy ByPass -NoProfile -File .\eng\build-uploader.ps1
-powershell -ExecutionPolicy ByPass -NoProfile -File .\eng\build-infra.ps1
-powershell -ExecutionPolicy ByPass -NoProfile -File .\eng\build-comment-function.ps1
+del /s /f /Q %~dp0\artifacts
+
+powershell -ExecutionPolicy ByPass -NoProfile -File %~dp0\eng\build-blog.ps1
+powershell -ExecutionPolicy ByPass -NoProfile -File %~dp0\eng\build-uploader.ps1
+powershell -ExecutionPolicy ByPass -NoProfile -File %~dp0\eng\build-infra.ps1
+powershell -ExecutionPolicy ByPass -NoProfile -File %~dp0\eng\build-comment-function.ps1
