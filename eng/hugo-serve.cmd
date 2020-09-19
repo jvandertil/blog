0ea631634876b1c35ee0a88b6a7a8b28f1781906
Jos van der Til <jos@vandertil.net>
2020-09-19 13:00:55 +0200

    Fix path detection in hugo-serve.cmd

--- eng/hugo-serve.cmd
+++ eng/hugo-serve.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -File ..\src\blog\hugo.ps1 serve -DEF --minify --source ..\src\blog
+powershell -ExecutionPolicy ByPass -NoProfile -File %~dp0\..\src\blog\hugo.ps1 serve -DEF --minify --source %~dp0\..\src\blog
 exit /b %ERRORLEVEL%
