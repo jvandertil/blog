f2741579d0235c4232a4a85839bd6731c9d45397
Jos van der Til <jos@vandertil.net>
2021-04-08 15:59:20 +0200

    Only publish production code

--- eng/build-comment-function.ps1
+++ eng/build-comment-function.ps1
@@ -4,7 +4,7 @@ $artifacts = Join-Path $root "artifacts"
 $source = Join-Path $root "src"
 
 $artifact = Join-Path $artifacts "blog-comment-function"
-$solution = Join-Path $source "blog-comment-function" | Join-Path -ChildPath "BlogComments.sln"
+$solution = Join-Path $source "blog-comment-function" | Join-Path -ChildPath "src" | Join-Path -ChildPath "BlogComments" | Join-Path -ChildPath "BlogComments.csproj"
 
 dotnet build -c Release $solution
 dotnet test --no-build -c Release $solution
