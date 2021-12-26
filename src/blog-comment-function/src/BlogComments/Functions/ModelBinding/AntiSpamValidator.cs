26c7468664e79829eb643ea67affc94e2ff31bfc
Jos van der Til <jos@vandertil.net>
2021-12-26 01:12:48 +0100

    Update BlogComments to .NET 6

--- src/blog-comment-function/src/BlogComments/Functions/ModelBinding/AntiSpamValidator.cs
+++ src/blog-comment-function/src/BlogComments/Functions/ModelBinding/AntiSpamValidator.cs
@@ -1,9 +1,11 @@
 using System;
+using BlogComments.Functions.Persistence;
+using FluentValidation;
 using FluentValidation.Validators;
 
 namespace BlogComments.Functions.ModelBinding
 {
-    public class AntiSpamValidator : PropertyValidator
+    public class AntiSpamValidator : PropertyValidator<CommentContents, string>
     {
         private static readonly string[] SpamTerms =
         {
@@ -12,25 +14,24 @@ namespace BlogComments.Functions.ModelBinding
             "azithromycin",
         };
 
-        protected override string GetDefaultMessageTemplate()
-        {
-            return "Do not post spam.";
-        }
+        public override string Name => nameof(AntiSpamValidator);
 
-        protected override bool IsValid(PropertyValidatorContext context)
+        public override bool IsValid(ValidationContext<CommentContents> context, string value)
         {
-            if (context.PropertyValue is string contents)
+            foreach (var term in SpamTerms)
             {
-                foreach (var term in SpamTerms)
+                if (value.Contains(term, StringComparison.OrdinalIgnoreCase))
                 {
-                    if (contents.Contains(term, StringComparison.OrdinalIgnoreCase))
-                    {
-                        return false;
-                    }
+                    return false;
                 }
             }
 
             return true;
+        }
+
+        protected override string GetDefaultMessageTemplate(string errorCode)
+        {
+            return "Do not post spam.";
         }
     }
 }
