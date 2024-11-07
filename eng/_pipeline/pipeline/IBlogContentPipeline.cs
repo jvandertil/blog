98143db23e654ee6609ca871287833db6053ccd5
Jos van der Til <jos@vandertil.net>
2024-11-07 11:53:03 +0100

    Bump Hugo to 0.138.0

--- eng/_pipeline/pipeline/IBlogContentPipeline.cs
+++ eng/_pipeline/pipeline/IBlogContentPipeline.cs
@@ -8,7 +8,7 @@ namespace Vandertil.Blog.Pipeline
 {
     public interface IBlogContentPipeline : IProvideArtifactsDirectory, IProvideSourceDirectory
     {
-        private const string HugoVersion = "0.134.2";
+        private const string HugoVersion = "0.138.0";
 
         private AbsolutePath ContentSourceDirectory => SourceDirectory / "blog";
 
