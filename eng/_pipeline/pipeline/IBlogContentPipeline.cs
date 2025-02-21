2d8b35733f20da56eb9967b4705b9293a3e3430c
Jos van der Til <jos@vandertil.net>
2025-02-21 16:25:44 +0100

    Update packages

--- eng/_pipeline/pipeline/IBlogContentPipeline.cs
+++ eng/_pipeline/pipeline/IBlogContentPipeline.cs
@@ -8,7 +8,7 @@ namespace Vandertil.Blog.Pipeline
 {
     public interface IBlogContentPipeline : IProvideArtifactsDirectory, IProvideSourceDirectory
     {
-        private const string HugoVersion = "0.140.2";
+        private const string HugoVersion = "0.144.1";
 
         private AbsolutePath ContentSourceDirectory => SourceDirectory / "blog";
 
