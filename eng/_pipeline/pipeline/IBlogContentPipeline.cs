c2ee429b792dbdd353dbea1e8f09251c1e913c04
Jos van der Til <jos@vandertil.net>
2025-02-22 01:31:04 +0100

    Update Hugo to 0.145.0

--- eng/_pipeline/pipeline/IBlogContentPipeline.cs
+++ eng/_pipeline/pipeline/IBlogContentPipeline.cs
@@ -8,7 +8,7 @@ namespace Vandertil.Blog.Pipeline
 {
     public interface IBlogContentPipeline : IProvideArtifactsDirectory, IProvideSourceDirectory
     {
-        private const string HugoVersion = "0.144.1";
+        private const string HugoVersion = "0.145.0";
 
         private AbsolutePath ContentSourceDirectory => SourceDirectory / "blog";
 
