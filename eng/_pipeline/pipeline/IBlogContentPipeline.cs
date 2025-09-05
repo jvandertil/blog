2dda2ae48c7497bef6183fd5e9b176dcc212fca5
jvandertil <158879+jvandertil@users.noreply.github.com>
2025-09-05 03:16:29 +0000

    chore: update HugoVersion to 0.149.1

--- eng/_pipeline/pipeline/IBlogContentPipeline.cs
+++ eng/_pipeline/pipeline/IBlogContentPipeline.cs
@@ -8,7 +8,7 @@ namespace Vandertil.Blog.Pipeline
 {
     public interface IBlogContentPipeline : IProvideArtifactsDirectory, IProvideSourceDirectory
     {
-        private const string HugoVersion = "0.149.0";
+        private const string HugoVersion = "0.149.1";
 
         private AbsolutePath ContentSourceDirectory => SourceDirectory / "blog";
 
